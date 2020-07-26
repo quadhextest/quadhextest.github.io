c8a28116cbcc36bbbb7a501767e5c838301943af
Megasteel32 <megasteel32@gmail.com>
2020-07-26 13:49:57 -0400

    edited questions.js to set the stage

---
+++ questions.js
@@ -0,0 +1,18 @@
+questions = [
+    {
+        "question": "Bitches ain't shit",
+        "score_effect": {
+            "Religious" : 1,
+            "Tradition" : 2,
+            "Respect"   : 3,
+            "Control"   : 4,
+            "Authority" : 5,
+            "Federation": 6,
+            "Equality"  : 7,
+            "Hardship"  : 8,
+            "Charity"   : 9,
+            "War"       : 10,
+            "Seclusion" : 11,
+            "Isolation" : 12,
+        }
+    },
