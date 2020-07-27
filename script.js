7a080f932f1b9f73458ee27ab06d78b33f95ea57
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-26 22:18:21 -0400

    YOOOOOOOOOOOOOO

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 window.onload = function () {
+    var line = parseInt(prompt("Please choose an axis: 1, 2, or 3 (Default is 1)"));
+
+    if (!line || ![1,2,3].includes(line)) {
+        line = 1;
+    }
+
+    var score = parseInt(prompt("Please enter a score between -50 and 50 (Default is 0)"));
+
+    if (!score || score > 50 || score < -50) {
+        score = 0;
+    }
+
     var ctx = document.getElementById('graph').getContext('2d');
 
-    var boundaries = [[-16.5, 41], [16.5, 41], [31, -1.5], [16.5, -44], [16.5, -44], [-32.2, -1.5]];
+    var boundaries = [
+        [-0.5, 47], [-0.5, -49],
+        [23, 22.5], [-24, -25],
+        [23, -25],  [-24, 22]
+    ];
 
-    var hexagon = new Chart(ctx, {
+    function scaleData(axis, w) {
+        switch (axis) {
+            case 1:
+                var x = -0.5;
+                var y = 96 * ((w + 50) / 100) - 49;
+                break;
+            case 2:
+                var x = 47 * ((w + 50) / 100) - 24;
+                var y = 47.5 * ((w + 50) / 100) - 25;
+                break;
+            case 3:
+                var x = 47 * ((w + 50) / 100) - 24;
+                var y = -47 * ((w + 50) / 100) + 22;
+                break;
+        }
+
+        console.log(w, x, y);
+
+        return [{
+            x: x,
+            y: y
+        }];
+    }
+
+    window.hexagon = new Chart(ctx, {
         type: 'scatter',
         data: {
             datasets: [{
@@ -12,10 +52,7 @@ window.onload = function () {
                 pointBorderColor: '#FF0000',
                 pointBorderWidth: 20,
                 backgroundColor: 'rgb(255, 99, 132)',
-                data: [{
-                    x: 16.5,
-                    y: -44
-                }]
+                data: scaleData(line, score)
             }]
         },
 
@@ -26,7 +63,7 @@ window.onload = function () {
             tooltips: {
                 callbacks: {
                     label: function (tooltipItem) {
-                        return tooltipItem.yLabel;
+                        return tooltipItem.xLabel.toFixed(2) + ", " + tooltipItem.yLabel.toFixed(2);
                     }
                 }
             },
