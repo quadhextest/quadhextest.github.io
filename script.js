da19bf32a6c18b243e701cbdd240eda8c0d879dd
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-27 22:16:42 -0400

    graphing works yay! (also suppressed some logs)

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 window.onload = function () {
     Score();
-    Hexagon();
+    Hexagon("graph0");
+    Hexagon("graph1");
+    Hexagon("graph2");
+    Hexagon("graph3");
 }
 
 function Score() {
-    var Religious = 0;
-    var Tradition = 0;
-    var Respect = 0;
-    var Control = 0;
-    var Authority = 0;
-    var Federation = 0;
-    var Equality = 0;
-    var Hardship = 0;
-    var Charity = 0;
-    var War = 0;
-    var Seclusion = 0;
-    var Isolation = 0;
-
-    // var score = [Religious,Tradition,Respect,Control,Authority,Federation,Equality,Hardship,Charity,War,Seclusion,Isolation];
     var score = [0,0,0,0,0,0,0,0,0,0,0,0];
 
     for (let i = 0; i <= 20; i++) {
         var questionString = questions[i].question;
         var scoreEffectMap = questions[i].score_effect;
-
 
         var scoreEffectArray = [];
 
@@ -32,39 +20,27 @@ function Score() {
         }
 
         var seed = Math.floor(5 * Math.random());
-        var weight = [-1, -0.5, 0, 0.5, 1][seed];
+        var weight = [-1, -0.5, 0, 0.5, 1][seed]; // later replace this with a weight array in function params that is based off of user answers
 
         for (let j = 0; j < 12; j++) {
-            score[j] += weight * scoreEffectArray[j];
+            score[j] += weight * scoreEffectArray[j]; // weight[j] (array of weights based off of user answers)
         }
 
-        console.log("Question:", questionString);
-        console.log("Score Effect:", scoreEffectArray);
-        console.log("Weight:", weight);
-        console.log("New Score Array:", score);
-
-        console.log("---------");
+        // console.log("Question:", questionString);
+        // console.log("Score Effect:", scoreEffectArray);
+        // console.log("Weight:", weight);
+        // console.log("New Score Array:", score);
+        // console.log("---------");
     }
 }
 
-function Hexagon() {
-    var scores = [];
-    var totalScore = 0;
+function Hexagon(id) {
+    var s1 = generateScores();
+    var s2 = generateScores();
+    var s3 = generateScores();
 
-    for (var i = 0; i < 33; i++) {
-        var newScore = 50 * (Math.random() - Math.random());
-        scores.push(newScore);
-        totalScore += newScore;
-    }
-
-    var score = totalScore / 33;
-    
-    var line = Math.ceil(Math.random() * 3);
-
-    document.getElementById("results").innerHTML = `Axis: ${line}<br>Total Score: ${totalScore}<br>Final Score: ${score}`;
-    document.getElementById("scores").innerHTML = `Scores:<br>${scores.toString().replace(/,/g, "<br>")}`;
-
-    var ctx = document.getElementById('graph').getContext('2d');
+    var result = combineScores(s1, s2, s3);
+    console.log(id, result[0]);
 
     var a = [-24, 22];
     var b = [-0.5, 47];
@@ -73,31 +49,9 @@ function Hexagon() {
     var bx = [-0.5, -49];
     var cx = [-24, -25];
 
-    function scaleData(axis, w) {
-        switch (axis) {
-            case 1:
-                var x = -47 * ((w + 50) / 100) + 23;
-                var y = 47 * ((w + 50) / 100) - 25;
-                break;
-            case 2:
-                var x = -0.5;
-                var y = 96 * ((w + 50) / 100) - 49;
-                break;
-            case 3:
-                var x = 47 * ((w + 50) / 100) - 24;
-                var y = 47.5 * ((w + 50) / 100) - 25;
-                break;
-        }
+    var ctx = document.getElementById(id).getContext('2d');
 
-        document.getElementById("results").innerHTML += `<br>Graph X-value: ${x}<br>Graph Y-value: ${y}`;
-
-        return [{
-            x: x,
-            y: y
-        }];
-    }
-
-    var hexagon = new Chart(ctx, {
+    let hexagon = new Chart(ctx, {
         type: 'scatter',
         data: {
             datasets: [{
@@ -106,7 +60,7 @@ function Hexagon() {
                 pointBorderColor: '#FF0000',
                 pointBorderWidth: 20,
                 backgroundColor: 'rgb(255, 99, 132)',
-                data: scaleData(line, score)
+                data: result
             }]
         },
 
@@ -124,27 +78,83 @@ function Hexagon() {
             scales: {
                 yAxes: [{
                     ticks: {
-                        display: false,
+                        // display: false,
                         min: -50,
                         max: 50,
                     },
-                    gridLines: {
-                        display: false,
-                        drawBorder: false,
-                    },
+                    // gridLines: {
+                    //     display: false,
+                    //     drawBorder: false,
+                    // },
                 }],
                 xAxes: [{
                     ticks: {
-                        display: false,
+                        // display: false,
                         min: -50,
                         max: 50,
                     },
-                    gridLines: {
-                        drawBorder: false,
-                        display: false,
-                    },
+                    // gridLines: {
+                    //     drawBorder: false,
+                    //     display: false,
+                    // },
                 }],
             },
         },
     });
 };
+
+function generateScores() {
+    var scores = [];
+    var totalScore = 0;
+
+    for (var i = 0; i < 33; i++) {
+        var newScore = 50 * (Math.random() - Math.random());
+        scores.push(newScore);
+        totalScore += newScore;
+    }
+
+    var score = totalScore / 33;
+
+    return score;
+}
+
+function scaleScores(axis, w) {
+    switch (axis) {
+        case 1:
+            var x = -47 * ((w + 50) / 100) + 23;
+            var y = 47 * ((w + 50) / 100) - 25;
+            break;
+        case 2:
+            var x = -0.5;
+            var y = 96 * ((w + 50) / 100) - 49;
+            break;
+        case 3:
+            var x = 47 * ((w + 50) / 100) - 24;
+            var y = 47.5 * ((w + 50) / 100) - 25;
+            break;
+    }
+
+    return [x, y];
+}
+
+function combineScores(s1, s2, s3) {
+    var ss1 = scaleScores(1, s1);
+    var ss2 = scaleScores(2, s2);
+    var ss3 = scaleScores(3, s3);
+
+    var ax = ss1[0];
+    var ay = ss1[1];
+    var bx = ss2[0];
+    var by = ss2[1];
+    var cx = ss3[0];
+    var cy = ss3[1];
+
+    var d = 2 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by));
+    var ux = ((ax * ax + ay * ay) * (by - cy) + (bx * bx + by * by) * (cy - ay) + (cx * cx + cy * cy) * (ay - by)) / d;
+    var uy = ((ax * ax + ay * ay) * (cx - bx) + (bx * bx + by * by) * (ax - cx) + (cx * cx + cy * cy) * (bx - ax)) / d;
+
+    return [{
+        x: ux,
+        y: uy
+    }];
+}
